7147fbb758e64017f47ede6ae2f9ec5b09fe7675
winsoft666 <winsoft666@outlook.com>
2019-03-07 17:10:51 +0800

    test source file.

---
+++ vsproject/test/test-01/DisplayMonitorTest.cpp
@@ -0,0 +1,23 @@
+﻿#include "stdafx.h"
+#include "DisplayMonitorTest.h"
+
+
+void DisplayMonitorTest() {
+    ppx::base::DisplayMonitors dd;
+    bool ret = dd.EnumDisplayMonitors();
+
+    for (int i = 0; i < dd.GetCount(); i++) {
+        ppx::base::DSPMONITOR dev;
+        if (dd.GetDspInfo(i, &dev)) {
+            printf("\n[%d]: primary:%d, scale=%d,   x=%d, y=%d, width=%d, height=%d, workarea_x=%d, workarea_x=%d, workarea_width=%d, workarea_height=%d\n==============\n",
+                i,
+                dev.primary,
+                dev.scale,
+                dev.x, dev.y, dev.width, dev.height,
+                dev.workarea_x, dev.workarea_y, dev.workarea_width, dev.workarea_height
+            );
+        }
+    }
+
+}
+

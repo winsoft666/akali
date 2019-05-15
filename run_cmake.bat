3535eb96b695eff9626528de4f6be6b13fb43401
winsoft666 <winsoft666@outlook.com>
2019-05-15 14:33:31 +0800

    update cmake script.

---
+++ run_cmake.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Program Files\CMake\bin\cmake.exe" -G "Visual Studio 14" -DCMAKE_TOOLCHAIN_FILE=D:\sourcecode\vcpkg\scripts\buildsystems\vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x86-windows -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=D:\ppx -DPPX_NO_HTTP=OFF -S %~dp0 -B %~dp0build

8fe80fef9ebecc46a9b275cc96f8f81eefeb758b
winsoft666 <winsoft666@outlook.com>
2019-05-20 23:55:07 +0800

    add build.bat

---
+++ build.bat
@@ -0,0 +1,8 @@
+@echo off
+vcpkg install curl[openssl]:x86-windows
+vcpkg install cryptopp:x86-windows
+vcpkg install gtest:x86-windows
+
+cmake.exe -G "Visual Studio 14" -DCMAKE_TOOLCHAIN_FILE=D:\sourcecode\vcpkg\scripts\buildsystems\vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x86-windows -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=D:\ppx -DPPX_NO_HTTP=OFF -DPPX_NO_ENCRYPT=OFF -DBUILD_TESTS=ON -S %~dp0 -B %~dp0build
+
+cmake.exe --build %~dp0build

04fdff9fada4a44e8c388b19ff17b667e3ff017b
winsoft666 <winsoft666@outlook.com>
2019-05-21 11:52:00 +0800

    bat update.

--- run_cmake.bat
+++ run_cmake.bat
@@ -1,2 +1,2 @@
 @echo off
-"C:\Program Files\CMake\bin\cmake.exe" -G "Visual Studio 14" -DCMAKE_TOOLCHAIN_FILE=D:\sourcecode\vcpkg\scripts\buildsystems\vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x86-windows -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=D:\ppxbase -DBUILD_TESTS=ON -S %~dp0 -B %~dp0build
+cmake.exe -G "Visual Studio 14" -DCMAKE_TOOLCHAIN_FILE=D:\sourcecode\vcpkg\scripts\buildsystems\vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x86-windows -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=D:\ppxbase -DBUILD_TESTS=ON -S %~dp0 -B %~dp0build

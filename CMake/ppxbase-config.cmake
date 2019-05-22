b46ec0aeecfa351faa81483656b3e78fb09a8286
winsoft666 <winsoft666@outlook.com>
2019-05-22 16:57:28 +0800

    UPDATE CMakeLists.txt

--- CMake/ppxbase-config.cmake
+++ CMake/ppxbase-config.cmake
@@ -1,4 +1,13 @@
 # ppxbase-config.cmake - package configuration file
 
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-include(${SELF_DIR}/ppxbase-target-${CMAKE_BUILD_TYPE}.cmake)
+
+if(CMAKE_BUILD_TYPE STREQUAL "" OR NOT DEFINED CMAKE_BUILD_TYPE)
+	include(${SELF_DIR}/ppxbase-target.cmake)
+else()
+	include(${SELF_DIR}/ppxbase-target-${CMAKE_BUILD_TYPE}.cmake)
+endif()
+
+
+get_target_property(PPXBASE_INCLUDE_DIRS ppxbase INTERFACE_INCLUDE_DIRECTORIES)
+get_target_property(PPXBASE_LIBRARIES ppxbase "LOCATION${CMAKE_BUILD_TYPE}")

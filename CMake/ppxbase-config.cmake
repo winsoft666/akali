0043e49bc55250a1724613304cbb58147520005a
winsoft666 <winsoft666@outlook.com>
2019-05-23 15:57:06 +0800

    compile script.

--- CMake/ppxbase-config.cmake
+++ CMake/ppxbase-config.cmake
@@ -4,9 +4,5 @@ get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 
 
 include(${SELF_DIR}/ppxbase-target.cmake)
+include(${SELF_DIR}/ppxbase-static-target.cmake)
 
-
-if(TARGET ppxbase)
-	get_target_property(PPXBASE_INCLUDE_DIRS ppxbase INTERFACE_INCLUDE_DIRECTORIES)
-	get_target_property(PPXBASE_LIBRARIES ppxbase "IMPORTED_IMPLIB_${CMAKE_BUILD_TYPE}")
-endif()

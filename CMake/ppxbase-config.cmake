faa7201b711a94010314dc4a89693c12ef73e0d3
winsoft666 <winsoft666@outlook.com>
2019-05-23 16:53:19 +0800

    check if cmake file exist.

--- CMake/ppxbase-config.cmake
+++ CMake/ppxbase-config.cmake
@@ -2,7 +2,10 @@
 
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 
+if(EXISTS ${SELF_DIR}/ppxbase-target.cmake)
+	include(${SELF_DIR}/ppxbase-target.cmake)
+endif()
 
-include(${SELF_DIR}/ppxbase-target.cmake)
-include(${SELF_DIR}/ppxbase-static-target.cmake)
-
+if(EXISTS ${SELF_DIR}/ppxbase-static-target.cmake)
+	include(${SELF_DIR}/ppxbase-static-target.cmake)
+endif()

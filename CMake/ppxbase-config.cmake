a41a418154d0c403e0aac039e419b1d30556266d
winsoft666 <winsoft666@outlook.com>
2019-05-23 10:13:23 +0800

    support vcpkg.

--- CMake/ppxbase-config.cmake
+++ CMake/ppxbase-config.cmake
@@ -2,13 +2,10 @@
 
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 
-if(CMAKE_BUILD_TYPE STREQUAL "" OR NOT DEFINED CMAKE_BUILD_TYPE)
-	include(${SELF_DIR}/ppxbase-target.cmake)
-else()
-	include(${SELF_DIR}/ppxbase-target-${CMAKE_BUILD_TYPE}.cmake)
-endif()
+
+include(${SELF_DIR}/ppxbase-target.cmake)
 
 if(TARGET ppxbase)
 	get_target_property(PPXBASE_INCLUDE_DIRS ppxbase INTERFACE_INCLUDE_DIRECTORIES)
-	get_target_property(PPXBASE_LIBRARIES ppxbase "LOCATION${CMAKE_BUILD_TYPE}")
+	get_target_property(PPXBASE_LIBRARIES ppxbase "LOCATION_${CMAKE_BUILD_TYPE}")
 endif()

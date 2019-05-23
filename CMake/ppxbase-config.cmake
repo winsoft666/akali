fa74dcd0acc86c34a14c40a46701d1a5db9c4dcc
winsoft666 <winsoft666@outlook.com>
2019-05-23 12:33:16 +0800

    fix link file error.

--- CMake/ppxbase-config.cmake
+++ CMake/ppxbase-config.cmake
@@ -5,7 +5,8 @@ get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 
 include(${SELF_DIR}/ppxbase-target.cmake)
 
+
 if(TARGET ppxbase)
 	get_target_property(PPXBASE_INCLUDE_DIRS ppxbase INTERFACE_INCLUDE_DIRECTORIES)
-	get_target_property(PPXBASE_LIBRARIES ppxbase "LOCATION_${CMAKE_BUILD_TYPE}")
+	get_target_property(PPXBASE_LIBRARIES ppxbase "IMPORTED_IMPLIB_${CMAKE_BUILD_TYPE}")
 endif()
